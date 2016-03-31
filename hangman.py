e0aed9ef6b399acc3eaa5cec6cd0b1882d62d7d1
prisbre <huanfeng.c@gmail.com>
2016-03-31 21:53:53 +0800

    feat: def loadWords()

--- hangman.py
+++ hangman.py
@@ -4,12 +4,26 @@ import random
 import string
 import os.path
 
-WORDLIST_FILENAME = str(os.path.dirname(__file__)) + r"\words.txt"
+fileName = str(os.path.dirname(__file__)) + r"\words.txt"
 
-def loadWords(WORDLIST_FILENAME):
+def loadWords(fileName):
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print "Loading word list from file..."
+    fileHandle = open(fileName, 'r')
+    line = fileHandle.readline()
+    wordlist = string.split(line)
+    print "  ", len(wordlist), "words loaded."
+    return wordlist
+
 
 def chooseWord(wordlist):
 
+'''
 def isLegal(input):
 
 def isWordGuessed(letterGuessed, target):
@@ -20,4 +34,6 @@ def narrowRange(letterGuessed):
 
 
 def hangman():
+'''
 
+hangman()
