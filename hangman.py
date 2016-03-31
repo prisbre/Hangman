3fdf3a65d2d340b2549d0bddd21d4501272dd2f6
prisbre <huanfeng.c@gmail.com>
2016-03-31 21:20:06 +0800

    hangman.py first commit

---
+++ hangman.py
@@ -0,0 +1,23 @@
+# Hangman game
+
+import random
+import string
+import os.path
+
+WORDLIST_FILENAME = str(os.path.dirname(__file__)) + r"\words.txt"
+
+def loadWords(WORDLIST_FILENAME):
+
+def chooseWord(wordlist):
+
+def isLegal(input):
+
+def isWordGuessed(letterGuessed, target):
+
+def getGuessedWord(letterGuessed, target):
+
+def narrowRange(letterGuessed):
+
+
+def hangman():
+
